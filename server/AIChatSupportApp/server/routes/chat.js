22ee10d3ff86ce70c98ad31aefaf7ecd15914189
SridharSolaris <21A91A05I2@aec.edu.in>
2025-07-01 09:54:35 +0530

    Clean up project: Remove unnecessary configurations

--- server/AIChatSupportApp/server/routes/chat.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const chatController = require("../controllers/chatController");
-
-router.post("/", chatController.sendMessage);
-router.get("/", chatController.getConversations);
-router.get("/:id", chatController.getConversation);
-
-module.exports = router;
--- server/AIChatSupportApp/server/routes/upload.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const uploadController = require("../controllers/uploadController");
-const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
-
-router.post("/", upload.single("file"), uploadController.uploadFile);
-
-module.exports = router;
